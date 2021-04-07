c4035a118cfd096ac55497cc53873225c4a2037b
evitla <s.dinmuhamed@gmail.com>
2021-04-07 17:22:59 +0600

    feat: solve matrix-elements-sum problem

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,14 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  return matrix.reduce((acc, row, i) => {
+    if (i === 0) return row.reduce((a, b) => a + b, 0);
+    const prevRow = matrix[i - 1];
+    const rowSum = row.reduce((a, b, j) => (prevRow[j] !== 0 ? a + b : a), 0);
+
+    return acc + rowSum;
+  }, 0);
 }
 
 module.exports = getMatrixElementsSum;
