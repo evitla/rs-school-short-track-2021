d8e7c6afe1d060d805076c4500fb9dc986cf770c
evitla <s.dinmuhamed@gmail.com>
2021-04-07 21:31:53 +0600

    feat: solve common-character-count problem

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,21 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function countChars(str) {
+  return str.split('').reduce((obj, char) => {
+    // eslint-disable-next-line no-param-reassign
+    obj[char] = (obj[char] || 0) + 1;
+    return obj;
+  }, {});
+}
+
+function getCommonCharacterCount(s1, s2) {
+  const countS1 = countChars(s1);
+  const countS2 = countChars(s2);
+
+  return Object.keys(countS1).reduce((acc, char) => (
+    acc + (countS1[char] < countS2[char] ? countS1[char] : (countS2[char] || 0))
+  ), 0);
 }
 
 module.exports = getCommonCharacterCount;
