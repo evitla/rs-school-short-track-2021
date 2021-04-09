72971fb450bb8bf5a7e2a344711fa35e095833d1
evitla <s.dinmuhamed@gmail.com>
2021-04-10 01:01:10 +0600

    feat: solve encode-line problem

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+
+  let prevChar = '';
+  let counter = 0;
+  str.split('').forEach((char, idx) => {
+    if (char !== prevChar) {
+      result += (counter > 1 ? counter : '') + prevChar;
+      counter = 0;
+    }
+    counter++;
+    if (idx === str.length - 1) result += (counter > 1 ? counter : '') + char;
+    prevChar = char;
+  });
+
+  return result;
 }
 
 module.exports = encodeLine;
